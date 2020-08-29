da6237b2313e52aeb9e00028af15ef2ed9e24ce4
Hendra Dwi Saputra <hendra@usa.com>
2020-08-29 16:04:20 +0700

    Login model | updated

--- apps/login/models.py
+++ apps/login/models.py
@@ -5,5 +5,5 @@ from apps.user.models import User
 
 class Finger(models.Model):
     username = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
-    finger_id = models.IntegerField(max_length=11)
+    finger_id = models.IntegerField()
     finger_data = models.TextField()
