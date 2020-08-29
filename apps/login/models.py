762d30e7622d2979c1d656d698c8d696cf7008e4
Hendra Dwi Saputra <hendra@usa.com>
2020-08-29 15:43:23 +0700

    Login model | finger

--- apps/login/models.py
+++ apps/login/models.py
@@ -1,1 +1,9 @@
-# Create your models here.
+from django.db import models
+
+from apps.user.models import User
+
+
+class Finger(models.Model):
+    username = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    finger_id = models.IntegerField(max_length=11)
+    finger_data = models.TextField()
