ee625496ba74c51cf7a1a70d69f7d1c7b89fc4f1
Hendra Dwi Saputra <hendra@usa.com>
2020-09-03 06:54:50 +0700

    Device Application | Fixed

--- apps/device/urls.py
+++ apps/device/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from apps.device.views import device
+from apps.device.views import create, read, update, delete
 
 urlpatterns = [
-    path('', device, name='device'),
-    path('<str:function>/', device, name='device'),
+    path('create/', create, name='device-create'),
+    path('', read, name='device-read'),
+    path('update/<int:id_device>/', update, name='device-update'),
+    path('delete/<int:id_device>/', delete, name='device=delete'),
+
 ]
