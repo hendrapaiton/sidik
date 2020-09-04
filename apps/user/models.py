6f7ce221a5d267799075d4d211e3470d99724638
Hendra Dwi Saputra <hendra@usa.com>
2020-09-04 22:06:51 +0700

    Add finger model to User Application

--- apps/user/models.py
+++ apps/user/models.py
@@ -3,3 +3,9 @@ from django.db import models
 
 class User(models.Model):
     username = models.TextField(max_length=50, unique=True, null=False, blank=False)
+
+
+class Finger(models.Model):
+    username = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    finger_id = models.IntegerField()
+    finger_data = models.TextField()
